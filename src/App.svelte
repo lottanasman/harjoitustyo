4ca310fd362a31c8a53e40c96766c83e85676211
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-18 12:33:20 +0200

    appsvelte modified and new file TarotCard.svelte made

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
+  let numCards = 1;
 </script>
 
-<main></main>
+<main>
+  <h1>Tarot Card Reader</h1>
+</main>
 
 <style>
 </style>
---
+++ src/lib/TarotCard.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { fetchTarotCards, getCardImage } from './TarotCardFetch';
+
+  export let numCards;
+
+  let tarotCards = [];
+</script>
+
+<div>
+  <label>
+    Enter the number of cards you wish to draw!
+    <br />
+    <input type="number" bind:value={numCards} min="1" />
+  </label>
+</div>
