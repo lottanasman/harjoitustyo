f7bbf6823e4d2dfda11100d2f5609cc8fccc8ae1
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-18 18:08:32 +0200

    Modified CardList and added a new function to TarotCardFetch.js

--- src/lib/CardList.svelte
+++ src/lib/CardList.svelte
@@ -1,15 +1,27 @@
 <script>
   import Modal from './Modal.svelte';
+  import { fetchTarot, getCardImage } from './TarotCardFetch';
   import { createEventDispatcher } from 'svelte';
 
   export let showCardModal;
 
   const dispatch = createEventDispatcher();
   let tarotCards = [];
+
+  async function fetchCards() {
+    try {
+      tarotCards = await fetchTarot();
+      dispatch('avaa');
+    } catch (error) {
+      console.error('Error fetching tarot cards:', error);
+    }
+  }
 </script>
 
-{#if showCardModal}
-  <Modal></Modal>
-{/if}
+<button on:click={fetchCards}>Browse all cards</button>
 
-<button>Browse all cards</button>
+{#if showCardModal}
+  <Modal>
+    <div class="card"></div>
+  </Modal>
+{/if}
--- src/lib/TarotCardFetch.js
+++ src/lib/TarotCardFetch.js
@@ -17,3 +17,20 @@ export async function fetchTarotCards(numCards) {
 export function getCardImage(nameShort) {
   return `images/${nameShort}.jpg`;
 }
+
+//uusi haku koska yllä olevassa haetaan randomisti ja halusin että listaus on järjestyksessä
+export async function fetchTarot() {
+  try {
+    const response = await fetch(`https://tarotapi.dev/api/v1/cards/`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch tarot cards');
+    }
+
+    const data = await response.json();
+    return data.cards;
+  } catch (error) {
+    console.error('Error fetching tarot cards:', error);
+    throw error;
+  }
+}
