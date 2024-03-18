0ff4e340824d7144622ffa184c1bb79434ebb3ab
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-18 13:47:49 +0200

    Made Modal.svelte and modified TarotCard

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,35 @@
+<div class="backdrop" />
+
+<div class="modal">
+  <slot />
+  <footer>
+    <slot name="footer" />
+  </footer>
+</div>
+
+<style>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.75);
+    z-index: 10;
+  }
+
+  .modal {
+    padding: 1rem;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 40%;
+    height: 70%;
+    max-height: 80vh;
+    background-color: rgb(29, 28, 28);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    z-index: 100;
+    overflow: auto;
+  }
+</style>
--- src/lib/TarotCard.svelte
+++ src/lib/TarotCard.svelte
@@ -1,9 +1,26 @@
 <script>
   import { fetchTarotCards, getCardImage } from './TarotCardFetch';
+  import Modal from './Modal.svelte';
+  import { fade, scale } from 'svelte/transition';
 
   export let numCards;
 
   let tarotCards = [];
+  let showModal = false;
+
+  async function drawCards() {
+    try {
+      tarotCards = await fetchTarotCards(numCards);
+      showModal = true;
+      numCards = 1;
+    } catch (error) {
+      error = 'Error fetching tarot cards';
+    }
+  }
+
+  function close() {
+    showModal = false;
+  }
 </script>
 
 <div>
@@ -13,3 +30,55 @@
     <input type="number" bind:value={numCards} min="1" />
   </label>
 </div>
+
+<button on:click={drawCards}>Draw Cards</button>
+
+{#if showModal}
+  <Modal>
+    <div
+      class="card"
+      in:scale={{ duration: 500, opacity: 0.5, start: 0.5 }}
+      out:fade
+    >
+      {#if tarotCards.length > 0}
+        {#each tarotCards as card}
+          <div class="tarotCard">
+            <h2>{card.name}</h2>
+            <img src={getCardImage(card.name_short)} alt={card.name} />
+            <p>{card.desc}</p>
+          </div>
+        {/each}
+      {:else}
+        <div>Error fetching tarot cards</div>
+      {/if}
+    </div>
+    <button slot="footer" on:click={close}>Close</button>
+  </Modal>
+{/if}
+
+<style>
+  .tarotCard {
+    display: flex;
+    flex-direction: column; /* Set the flex-direction to column to stack elements vertically */
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  img {
+    width: 17em; /* Adjust the width of the image to make it bigger */
+    margin-bottom: 1em; /* Add some margin to separate the image from other details */
+  }
+
+  p {
+    font-weight: bold; /* Make the name text bold */
+    margin-bottom: 1em; /* Add some margin between the name and the image */
+  }
+
+  input {
+    width: 5%;
+  }
+
+  div {
+    padding: 2em;
+  }
+</style>
