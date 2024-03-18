ce921d1f978cf41cdb10d0426362748818777e74
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-18 13:52:37 +0200

    Modified App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
+  import TarotCard from './lib/TarotCard.svelte';
+
   let numCards = 1;
 </script>
 
 <main>
   <h1>Tarot Card Reader</h1>
+
+  <TarotCard {numCards} />
 </main>
 
 <style>
