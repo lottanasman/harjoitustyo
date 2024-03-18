7dc282e57c6bde3fe079bd2db463d9a6166e9dac
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-18 17:45:13 +0200

    New component CardList.svelte created

---
+++ src/lib/CardList.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Modal from './Modal.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let tarotCards = [];
+</script>
+
+<button>Browse all cards</button>
