9c3af979c58d61effb1a8792d399d91c6b17cb43
Lotta Näsman <ae3398@student.jamk.fi>
2024-03-20 19:27:02 +0200

    modified CardList. Listing now works.

--- src/lib/CardList.svelte
+++ src/lib/CardList.svelte
@@ -27,12 +27,44 @@
 
 {#if showCardModal}
   <Modal>
-    <div class="card"></div>
-    <h2>{tarotCards[currentCardIndex].name}</h2>
-    <img
-      src={getCardImage(tarotCards[currentCardIndex].name_short)}
-      alt={tarotCards[currentCardIndex].name}
-    />
-    <!-- yllä oleva img tägin sisältö TEKOÄLYÄ -->
+    <div class="card">
+      <h2>{tarotCards[currentCardIndex].name}</h2>
+      <img
+        src={getCardImage(tarotCards[currentCardIndex].name_short)}
+        alt={tarotCards[currentCardIndex].name}
+      />
+      <!-- yllä oleva img tägin sisältö TEKOÄLYÄ -->
+
+      <div class="text-container">
+        <p>
+          <strong>Tarot card description:</strong>
+          {tarotCards[currentCardIndex].desc}
+        </p>
+        <p>
+          <strong>Card meaning when drawn up:</strong>
+          {tarotCards[currentCardIndex].meaning_up}
+        </p>
+        <p>
+          <strong>Card meaning when drawn reversed:</strong>
+          {tarotCards[currentCardIndex].meaning_rev}
+        </p>
+      </div>
+    </div>
+    <button slot="footer" on:click={sulje}>Close</button>
   </Modal>
 {/if}
+
+<style>
+  .card {
+    padding: 1em;
+    border: 1px solid #ccc;
+    border-radius: 1em;
+    text-align: center;
+  }
+
+  .card img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 1em;
+  }
+</style>
